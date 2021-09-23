756fde0256ce66969c93c1c4f80139e057c8fa30
Tri Dung Nguyen / BBM3H19M <Tri_Dung.Nguyen@edu.bib.de>
2021-09-23 13:52:22 +0200

    Projekt angelegt

---
+++ Notizen/Notizen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notizen
+//
+//  Created by Tri Dung Nguyen / BBM3H19M on 23.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
