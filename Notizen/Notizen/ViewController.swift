eea2e7fcb2674ce7a08efd46484b7eebb04ab8d8
Tri Dung Nguyen / BBM3H19M <Tri_Dung.Nguyen@edu.bib.de>
2021-09-24 10:34:27 +0200

    Reinschreiben in den Main geändert Lyout geändert

--- Notizen/Notizen/ViewController.swift
+++ Notizen/Notizen/ViewController.swift
@@ -15,10 +15,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func erstellenNotiz() {
-        guard let vc = storyboard?.instantiateViewController(identifier: "neu") as? ReinschreibenNotiz else {
+        guard let vc = storyboard?.instantiateViewController(identifier: "neu") as? ReinschreibenNotitz else {
             return
         }
         vc.title = "Neue Notiz"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = {noteTitle, note in
+            self.models.append((title: noteTitle, note: note))
+            self.label.isHidden = true
+            self.table.isHidden = false
+            self.table.reloadData()
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,7 +33,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: <#T##IndexPath#>)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = models[indexPath.row].title
         cell.detailTextLabel?.text = models[indexPath.row].note
         return cell
