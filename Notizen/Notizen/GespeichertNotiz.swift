d839fcff604d8835381074ef1433dea6ff76deda
Projekt angelegt <Alexandra.Soglassova@edu.bib.de>
2021-09-24 09:04:38 +0200

    Änderungen

--- Notizen/Notizen/GespeichertNotiz.swift
+++ Notizen/Notizen/GespeichertNotiz.swift
@@ -7,8 +7,14 @@ class GespeichertNotiz: UIViewController {
     
     @IBOutlet var  GespeicherteNotizUberschrift: UITextView!
     
+    @IBOutlet var DeleteButton: UIButton!
+    
+    @IBAction func DeleteButton(_ sender: UIButton) {
+    }
+    
     public var NotizUberschrift: String = ""
     public var Notiz: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
